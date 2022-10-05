c9fdc8067157ea6c0065685c4647acb6a216ba5d
carolnavya <carolnavya1999@gmail.com>
2022-10-05 04:23:27 -0400

    Adding db management methods

--- producerUtil.py
+++ producerUtil.py
@@ -1,5 +1,5 @@
 
-import time
+import datetime
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -8,9 +8,6 @@ from botocore.exceptions import ClientError
 """
 status : 0 ->created, 1-> success, -1->fail, 3->inqueue
 """
-
-logger = logging.getLogger(__name__)
-
 class MessageDB:
     def __init__(self,dynamodb):
          self.dynamodb = dynamodb
@@ -19,6 +16,7 @@ class MessageDB:
     def connect(self):
         # Get the service resource.
         dynamodb = boto3.resource('dynamodb')
+        print("Connected to AWS DynamoDB")
         return dynamodb
     
     def exists(self, table_name):
@@ -40,54 +38,68 @@ class MessageDB:
             self.table = table
         return exists
 
-    def createDB(self, table_name):
+    def createTable(self, table_name):
+        if not self.exists(table_name):
+            # Create the DynamoDB table.
+            try:
+                self.table = self.dynamodb.create_table(
+                    TableName=table_name,
+                    AttributeDefinitions=[
+                        {'AttributeName': 'sentTo', 'AttributeType': 'S'},
+                        {'AttributeName': 'content', 'AttributeType': 'S'},
+                        {'AttributeName': 'status', 'AttributeType': 'N'},
+                        # {'AttributeName': 'createdDatetime', 'AttributeType': 'S'},
+                        # {'AttributeName': 'sendDatetime', 'AttributeType': 'S'},
+                        # {'AttributeName': 'updateDatetime', 'AttributeType': 'S'}
+                    ],
+                    KeySchema=[
+                        {'AttributeName': 'sentTo', 'KeyType': 'HASH'},
+                        {'AttributeName': 'content', 'KeyType': 'RANGE'},
+                        # {'AttributeName': 'status', 'KeyType': 'RANGE'},
+                        # {'AttributeName': 'createdDatetime', 'KeyType': 'RANGE'},
+                        # {'AttributeName': 'sendDatetime', 'KeyType': 'RANGE'},
+                        # {'AttributeName': 'updateDatetime', 'KeyType': 'RANGE'}
+                    ],
+                    GlobalSecondaryIndexes=[
+                        {
+                            'IndexName': 'activity',
+                            'KeySchema': [
+                                {'AttributeName': 'status', 'KeyType': 'HASH'},
+                            ],
+                            'Projection': {'ProjectionType': 'ALL'},
+                            'ProvisionedThroughput': {
+                                'ReadCapacityUnits': 400,
+                                'WriteCapacityUnits': 400
+                            }
+                        },
+                    ],
+                    BillingMode='PROVISIONED',
+                    ProvisionedThroughput={
+                        'ReadCapacityUnits': 500,
+                        'WriteCapacityUnits': 500
+                    }
+                )
 
-        # Create the DynamoDB table.
-        try:
-            self.table = self.dynamodb.create_table(
-                TableName=table_name,
-                KeySchema=[
-                    {'AttributeName': 'mssageID', 'KeyType': 'HASH'},
-                    {'AttributeName': 'sentBy', 'KeyType': 'RANGE'},
-                    {'AttributeName': 'content', 'KeyType': 'HASH'},
-                    {'AttributeName': 'status', 'KeyType': 'RANGE'},
-                    {'AttributeName': 'createdDatetime', 'KeyType': 'RANGE'},
-                    {'AttributeName': 'sendDatetime', 'KeyType': 'RANGE'},
-                    {'AttributeName': 'updateDatetime', 'KeyType': 'RANGE'}
-                ],
-                AttributeDefinitions=[
-                    {'AttributeName': 'mssageID', 'AttributeType': 'N'},
-                    {'AttributeName': 'sentBy', 'AttributeType': 'S'},
-                    {'AttributeName': 'content', 'AttributeType': 'S'},
-                    {'AttributeName': 'status', 'AttributeType': 'N'},
-                    {'AttributeName': 'createdDatetime', 'AttributeType': 'S'},
-                    {'AttributeName': 'sendDatetime', 'AttributeType': 'S'},
-                    {'AttributeName': 'updateDatetime', 'AttributeType': 'S'}
-                ],
-                ProvisionedThroughput={
-                    'ReadCapacityUnits': 150,
-                    'WriteCapacityUnits': 150
-                }
-            )
-            # Wait until the table exists.
-            self.table.wait_until_exists()
-            
-        except ClientError as err:
-                logger.error(
-                    "Couldn't create table %s. Here's why: %s: %s", self.table,
-                    err.response['Error']['Code'], err.response['Error']['Message'])
-                raise
-    
+                # Wait until the table exists.
+                self.table.wait_until_exists()
 
-        # Print out some data about the table.
-        print(self.table.item_count)
+                # Print out some data about the table.
+                print("Table contents:", self.table.item_count)
+                
+            except ClientError as err:
+                    logger.error(
+                        "Couldn't create table %s. Here's why: %s: %s", self.table,
+                        err.response['Error']['Code'], err.response['Error']['Message'])
+                    raise
+        
     
     def addMessage(self, phno, messagePayload):
         try:
-            currtime = time.time()
+            # add phno, message pair produced by Producer into the database
+            currtime = datetime.datetime()
             self.table.put_item(
                 Item={
-                    'sentBy': phno,
+                    'sentTo': phno,
                     'content': messagePayload,
                     'status': 0,
                     'createdDatetime':currtime,
@@ -103,8 +115,22 @@ class MessageDB:
     def updateMessage(self):
         pass
 
-    def deleteMessage(self):
-
-        pass
+    def deleteMessage(self, phno, messagePayload):
+        # deleting phno, message pair
+        try:
+            self.table.delete_item(Key={'sentTo': phno, 'content': messagePayload})
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete message pair (%s,%s). Here's why: %s: %s", phno, messagePayload,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+    
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    db = MessageDB(None)
+    if not db.dynamodb:
+        db.dynamodb = db.connect()
+        print(db.createTable("messages"))
+        print(db.table.item_count)
 
 
